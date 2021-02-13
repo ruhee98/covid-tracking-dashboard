d0120761ed5cad3cb63c329bd0d6c1b7d59f8013
Ruhee S <ruhee21@github.com>
2021-02-13 14:51:24 -0600

    covid-19 tracker

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <h1>
+            CountryPicker
+        </h1>
+    )
+}
+
+export default Country;
---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+export { default as Cards} from './Card/Cards';
+export { default as Chart} from './Chart/Chart';
+export { default as Country} from './Country/Country';
