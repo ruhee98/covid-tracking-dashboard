91c3d4a1b28b9581cd18e6df9d8505e7dc273590
Ruhee S <ruhee21@github.com>
2021-02-13 16:14:23 -0600

    changes to charts

--- src/api/index.js
+++ src/api/index.js
@@ -22,7 +22,12 @@ export const fetchDailyData = async () => {
   try {
     const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
 
-    return data.map(({ positive, recovered, death, dateChecked: date }) => ({ confirmed: positive, recovered, deaths: death, date }));
+    const modifiedData = data.map(({positive, recovered, death, dateChecked: date}) => (
+      { confirmed: positive, 
+        recovered,
+        deaths: death, 
+        date}))
+    return modifiedData;
   } catch (error) {
     return error;
   }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,7 +3,7 @@ import {fetchDailyData} from '../../api';
 import { Line, Bar} from 'react-chartjs-2';
 import styles from './Chart.module.css';
 const Chart = () => {
-    const [dailyData, setDailyData] = useState({});
+    const [dailyData, setDailyData] = useState([]);
     
     useEffect(() => {
         const fetchAPI = async () => {
@@ -12,14 +12,27 @@ const Chart = () => {
         console.log(dailyData);
 
         fetchAPI();
-    }, []);
+    });
 
     const lineChart = (
-        dailyData[0]?(
+        dailyData.length // 0
+        ? (
         <Line
           data={{
-              labels: '',
-              datasets: [{}, {}],
+              labels: dailyData.map(({date}) => new Date(date).toLocaleDateString()),
+              datasets: [{
+                  data: dailyData.map((data) => data.confirmed),
+                  label: 'Infected',
+                  borderColor: '#3333ff',
+                  fill: true,
+              },
+              {
+                  data: dailyData.map((data) => data.deaths),
+                  label: 'Deaths',
+                  borderColor: 'red',
+                  backgroundColor: 'rgb(255, 0,0,0.5)',
+                  fill: true,
+              }],
           }}
         />
         ) : null
@@ -29,9 +42,9 @@ const Chart = () => {
 
 
     return (
-        <h1>
-            Charts
-        </h1>
+        <div className= {styles.container}>
+            {lineChart}
+        </div>
     )
 }
 
